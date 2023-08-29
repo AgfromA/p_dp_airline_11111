c3653f25d3a79e51af69ec8a952b6958a1cfe6ed
timur <Timurkakha@gmail.com>
2023-08-29 12:55:36 +0300

    commit

--- airline-payments/src/main/java/app/config/SwaggerConfig.java
+++ airline-payments/src/main/java/app/config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package app.config;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,7 +8,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
 
 import java.util.HashSet;
 import java.util.List;
