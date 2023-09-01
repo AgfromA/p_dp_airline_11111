50e2b300a31f483d5c3885de3f4b204f65a8a536
timur <Timurkakha@gmail.com>
2023-09-01 19:04:19 +0300

    commit

--- airline-project/src/main/java/app/config/SwaggerConfig.java
+++ airline-project/src/main/java/app/config/SwaggerConfig.java
@@ -8,59 +8,10 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import java.util.Arrays;
-import java.util.List;
 @OpenAPIDefinition(info = @Info(title = "S7 Airlines API", version = "1.0.6",
         description = "UI для работы с API проекта S7 Airlines."))
 @Configuration
 public class SwaggerConfig {
-
-    private SecurityContext securityContext() {
-        return SecurityContext
-                .builder()
-                .securityReferences(defaultAuth())
-                .build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("S7 Airlines API")
-                .description("UI для работы с API проекта S7 Airlines.")
-                .version("1.0.6")
-                .build();
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header");
-    }
 
     @Bean
     public OpenAPI customizeOpenAPI() {
