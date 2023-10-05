3682261e7e6b3b343d45d4b1dc5bcebda1b22662
CCARNIFEXX <112640748+CCARNIFEXX@users.noreply.github.com>
2023-10-05 21:02:21 +0300

    #268 Генерация уникального идентификатора на api-gateway для заголовков

---
+++ airline-gateway/src/main/java/app/airlinegateway/testController.java
@@ -0,0 +1,16 @@
+package app.airlinegateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class testController {
+    @GetMapping("/test")
+    public String test() {
+        log.info("test");
+        return "test";
+    }
+
+}
