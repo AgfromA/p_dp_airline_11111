40ebb6b76a27a8f717925d2f11855cc8a5251231
YuriSolom <ziemssenjoach@gmail.com>
2023-10-25 11:53:49 +0300

    [321] finalized front

--- airline-project/src/test/java/app/controllers/SearchControllerIT.java
+++ airline-project/src/test/java/app/controllers/SearchControllerIT.java
@@ -1,18 +1,14 @@
 package app.controllers;
 
-import app.entities.account.search.Search;
-import app.enums.Airport;
-import app.mappers.DestinationMapper;
-import app.services.interfaces.DestinationService;
-import org.json.JSONObject;
+import app.dto.search.Search;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static app.enums.Airport.OMS;
+import static app.enums.Airport.VKO;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,34 +17,90 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class SearchControllerIT extends IntegrationTestBase {
 
     @Test
-    void CheckSearchResultNotFound() throws Exception {
-        Long id = 200L;
-        mockMvc.perform(get("http://localhost:8080/api/search/{id}", id))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    void CreateSearchResultCreate() throws Exception {
-        var from = Airport.VKO;
-        var to = Airport.OMS;
-        var search = new Search(from, to, LocalDate.of(2023, 04, 01), null, 1);
+    void CheckSearchResult() throws Exception {
+        var search = new Search(VKO, OMS, LocalDate.of(2023, 04, 01), null, 1);
         mockMvc.perform(post("http://localhost:8080/api/search")
                         .content(objectMapper.writeValueAsString(search))
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isOk());
     }
 
+    @Test
+    void CheckSearchFromIsNull() throws Exception {
+        var search = new Search(null, OMS, LocalDate.of(2023, 04, 01),
+                LocalDate.of(2023, 04, 01), 1);
+        mockMvc.perform(post("http://localhost:8080/api/search")
+                        .content(objectMapper.writeValueAsString(search))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void CheckSearchToIsNull() throws Exception {
+        var search = new Search(VKO, null, LocalDate.of(2023, 04, 01),
+                LocalDate.of(2023, 04, 01), 1);
+        mockMvc.perform(post("http://localhost:8080/api/search")
+                        .content(objectMapper.writeValueAsString(search))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void CheckSearchFromAndToAreNull() throws Exception {
+        var search = new Search(null, null, LocalDate.of(2023, 04, 01),
+                LocalDate.of(2023, 04, 01), 1);
+        mockMvc.perform(post("http://localhost:8080/api/search")
+                        .content(objectMapper.writeValueAsString(search))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void CheckSearchPassengerIsNull() throws Exception {
+        var search = new Search(VKO, OMS, LocalDate.of(2023, 04, 01),
+                LocalDate.of(2023, 04, 01), null);
+        mockMvc.perform(post("http://localhost:8080/api/search")
+                        .content(objectMapper.writeValueAsString(search))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void CheckSearchPassengerIsLessOne() throws Exception {
+        var search = new Search(VKO, OMS, LocalDate.of(2023, 04, 01),
+                LocalDate.of(2023, 04, 01), 0);
+        mockMvc.perform(post("http://localhost:8080/api/search")
+                        .content(objectMapper.writeValueAsString(search))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void CheckSearchReturnEarlierDeparture() throws Exception {
+        var search = new Search(VKO, OMS, LocalDate.of(2023, 04, 05),
+                LocalDate.of(2023, 04, 01), 2);
+        mockMvc.perform(post("http://localhost:8080/api/search")
+                        .content(objectMapper.writeValueAsString(search))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void CheckSearchDepartureDateIsNull() throws Exception {
+        var search = new Search(VKO, OMS, null, null, 2);
+        mockMvc.perform(post("http://localhost:8080/api/search")
+                        .content(objectMapper.writeValueAsString(search))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 
     @Test
     void CheckSearchNotFound() throws Exception {
-        var from = Airport.VKO;
-        var to = Airport.OMS;
-        var search = new Search(from, to, LocalDate.of(1999, 12, 01), null, 1);
+        var search = new Search(VKO, OMS, LocalDate.of(1999, 04, 01),
+                LocalDate.of(1999, 04, 05), 2);
         mockMvc.perform(post("http://localhost:8080/api/search")
                         .content(objectMapper.writeValueAsString(search))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
     }
-
-
 }
--- airline-project/src/test/resources/sqlQuery/create-search-before.sql
+++ airline-project/src/test/resources/sqlQuery/create-search-before.sql
@@ -4,7 +4,6 @@ VALUES (1, 'VKO', 'Внуково', 'Москва', 'Россия', 'GMT +3');
 INSERT INTO destination (id, airport_code, airport_name, city_name, country_name, timezone)
 VALUES (2, 'OMS', 'Омск', 'Омск', 'Россия', 'GMT +6');
 
-
 INSERT INTO category (id, category_type)
 VALUES (1, 'FIRST');
 INSERT INTO category (id, category_type)
@@ -14,14 +13,12 @@ VALUES (3, 'PREMIUM_ECONOMY');
 INSERT INTO category (id, category_type)
 VALUES (4, 'ECONOMY');
 
-
 INSERT INTO aircrafts (id, aircraft_number, model, model_year, flight_range)
 VALUES (1, '17000012', 'Embraer E170STD', 2002, 3800);
 INSERT INTO aircrafts (id, aircraft_number, model, model_year, flight_range)
 VALUES (2, '5134', 'Airbus A320-200', 2011, 4300);
 INSERT INTO aircrafts (id, aircraft_number, model, model_year, flight_range)
 VALUES (3, '35283', 'Boeing 737-800', 2008, 5765);
-
 
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
 VALUES (1, '1A', false, true,
@@ -68,21 +65,37 @@ VALUES (11, '2E', true, true,
         (SELECT category.id FROM category WHERE category.id = 4),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 3));
 
-
 INSERT INTO flights (id, code, arrival_date, departure_date, flight_status, aircraft_id, from_id, to_id)
 VALUES (1, 'MSKOMSK', '2023-04-01 13:40:00', '2023-04-01 12:40:00', 'ON_TIME',
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 1), 1, 2);
 
-INSERT INTO flight_seats (id, fare, is_registered, is_sold, flight_id, seat_id)
-VALUES (1, 500, true, false,
+INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
+VALUES (1, 500, false, false, false,
         (SELECT flights.id FROM flights WHERE flights.id = 1),
         (SELECT seats.id FROM seats WHERE seats.id = 1));
-INSERT INTO flight_seats (id, fare, is_registered, is_sold, flight_id, seat_id)
-VALUES (2, 600, true, true,
+INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
+VALUES (2, 600, false, false, false,
         (SELECT flights.id FROM flights WHERE flights.id = 1),
         (SELECT seats.id FROM seats WHERE seats.id = 2));
-INSERT INTO flight_seats (id, fare, is_registered, is_sold, flight_id, seat_id)
-VALUES (3, 650, true, true, (SELECT flights.id FROM flights WHERE flights.id = 1),
+INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
+VALUES (3, 650, false, false, false, (SELECT flights.id FROM flights WHERE flights.id = 1),
         (SELECT seats.id FROM seats WHERE seats.id = 3));
 
+INSERT INTO flights (id, code, arrival_date, departure_date, flight_status, aircraft_id, from_id, to_id)
+VALUES (2, 'OMSKMSK', '2023-04-01 13:40:00', '2023-04-01 12:40:00', 'ON_TIME',
+        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 2), 2, 1);
 
+INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
+VALUES (4, 500, false, false, false,
+        (SELECT flights.id FROM flights WHERE flights.id = 2),
+        (SELECT seats.id FROM seats WHERE seats.id = 4));
+
+INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
+VALUES (5, 600, false, false, false,
+        (SELECT flights.id FROM flights WHERE flights.id = 2),
+        (SELECT seats.id FROM seats WHERE seats.id = 5));
+
+INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
+VALUES (6, 650, false, false, false,
+        (SELECT flights.id FROM flights WHERE flights.id = 2),
+        (SELECT seats.id FROM seats WHERE seats.id = 6));
