0c38e60fec1d66c9d66df9af0bf542b663f30030
NeAndrey <ne.and.rey@mail.ru>
2023-11-08 19:39:49 +0300

    Begin make tests for controller2

--- airline-project/src/test/java/app/controllers/SearchControllerIT.java
+++ airline-project/src/test/java/app/controllers/SearchControllerIT.java
@@ -13,6 +13,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.time.LocalDate;
 
 import static app.enums.Airport.*;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -24,15 +26,71 @@ class SearchControllerIT extends IntegrationTestBase {
     @Autowired
     private SearchService searchService;
 
-    @DisplayName("Positive test - search return one direct depart flight whith 3 free seats and one direct return " +
-            "flight whith 3 free seats")
+
+    //1. В базе: один прямой рейс туда с наличием мест (3 свободных).
+//   Поиск: рейс туда (2023-04-01) без поиска обратного рейса для 2-х пассажиров
+    @DisplayName("1 test. Search return one direct depart flight with 3 free seats")
+    @Test
+    void shouldReturnOneDirectDepartFlightWithThreeSeats() throws Exception {
+
+        Airport airportFrom = VKO;
+        Airport airportTo = OMS;
+        LocalDate departureDate = LocalDate.of(2023, 04, 01);
+        LocalDate returnDate = null;
+        Integer numberOfPassengers = 2;
+
+        SearchResult result = searchService.search(
+                airportFrom, airportTo, departureDate, returnDate, numberOfPassengers
+        );
+
+        System.out.println(objectMapper.writeValueAsString(
+                searchService.search(airportFrom, airportTo, departureDate, returnDate, numberOfPassengers)));
+
+
+        mockMvc.perform(get("http://localhost:8080/api/search")
+                .param("airportFrom", String.valueOf(airportFrom))
+                .param("airportTo", String.valueOf(airportTo))
+                .param("departureDate", String.valueOf(departureDate))
+                .param("numberOfPassengers", String.valueOf(numberOfPassengers)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(
+                        searchService.search(airportFrom, airportTo, departureDate, returnDate, numberOfPassengers))
+                ))
+                .andExpect(jsonPath("$.search.departureDate").value(departureDate.toString()))
+                .andExpect(jsonPath("$.search.returnDate", nullValue()))
+                .andExpect(jsonPath("$.search.numberOfPassengers").value(numberOfPassengers))
+                .andExpect(jsonPath("$.departFlights.size()").value(1))
+                .andExpect(jsonPath("$.departFlights.[0].airportFrom").value(airportFrom.toString()))
+                .andExpect(jsonPath("$.departFlights.[0].airportTo").value(airportTo.toString()))
+                .andExpect(jsonPath("$.departFlights.[0].seats.size()",
+                        Matchers.greaterThanOrEqualTo(numberOfPassengers)
+                ))
+                .andExpect(jsonPath("$.departFlights.[0].seats.size()").value(3))
+                .andExpect(jsonPath("$.departFlights.[0].seats[*].[?(@.isRegistered == false)].isRegistered",
+                        Matchers.contains(false, false, false)
+                ))
+                .andExpect(jsonPath("$.departFlights.[0].seats[*].[?(@.isSold == false)].isSold",
+                        Matchers.contains(false, false, false)
+                ))
+                .andExpect(jsonPath("$.departFlights.[0].seats[*].[?(@.isBooked == false)].isBooked",
+                        Matchers.contains(false, false, false)
+                ))
+                .andExpect(jsonPath("$.returnFlights.size()").value(0));
+    }
+
+
+    //2. В базе: один прямой рейс туда и один прямой рейс обратно с наличием мест (3 свободных).
+//   Поиск: рейс туда (2023-04-01) и рейс обратно (2023-04-03) для 2-х пассажиров
+    @DisplayName("2 test. Search return one direct depart flight with 3 free seats and one direct return " +
+            "flight with 3 free seats")
     @Test
     void shouldReturnOneDirectDepartAndOneDirectReturnFlightsWithThreeSeatsOnEarch() throws Exception {
 
         Airport airportFrom = VKO;
         Airport airportTo = OMS;
         LocalDate departureDate = LocalDate.of(2023, 04, 01);
-        LocalDate returnDate = LocalDate.of(2023, 04, 05);
+        LocalDate returnDate = LocalDate.of(2023, 04, 03);
         Integer numberOfPassengers = 2;
 
         SearchResult result = searchService.search(
@@ -86,4 +144,6 @@ class SearchControllerIT extends IntegrationTestBase {
                         Matchers.contains(false, false, false)
                 ));
     }
+
+
 }
--- airline-project/src/test/resources/sqlQuery/create-search-before.sql
+++ airline-project/src/test/resources/sqlQuery/create-search-before.sql
@@ -1,8 +1,15 @@
-INSERT INTO destination (id, airport_code, airport_name, city_name, country_name, timezone)
-VALUES (1, 'VKO', 'Внуково', 'Москва', 'Россия', 'GMT +3');
+-- Создаем 4 аэропорта
 
 INSERT INTO destination (id, airport_code, airport_name, city_name, country_name, timezone)
+VALUES (1, 'VKO', 'Внуково', 'Москва', 'Россия', 'GMT +3');
+INSERT INTO destination (id, airport_code, airport_name, city_name, country_name, timezone)
 VALUES (2, 'OMS', 'Омск', 'Омск', 'Россия', 'GMT +6');
+INSERT INTO destination (id, airport_code, airport_name, city_name, country_name, timezone)
+VALUES (3, 'KZN', 'Казань', 'Казань', 'Россия', 'GMT +3');
+INSERT INTO destination (id, airport_code, airport_name, city_name, country_name, timezone)
+VALUES (4, 'KUF', 'Курумоч', 'Самара', 'Россия', 'GMT +4');
+
+-- Создаем категории мест
 
 INSERT INTO category (id, category_type)
 VALUES (1, 'FIRST');
@@ -13,124 +20,123 @@ VALUES (3, 'PREMIUM_ECONOMY');
 INSERT INTO category (id, category_type)
 VALUES (4, 'ECONOMY');
 
+-- Создаем 4 aircrafts
+
 INSERT INTO aircrafts (id, aircraft_number, model, model_year, flight_range)
 VALUES (1, '17000012', 'Embraer E170STD', 2002, 3800);
 INSERT INTO aircrafts (id, aircraft_number, model, model_year, flight_range)
 VALUES (2, '5134', 'Airbus A320-200', 2011, 4300);
 INSERT INTO aircrafts (id, aircraft_number, model, model_year, flight_range)
 VALUES (3, '35283', 'Boeing 737-800', 2008, 5765);
+INSERT INTO aircrafts (id, aircraft_number, model, model_year, flight_range)
+VALUES (4, '31334', 'Boeing 737-900ER', 2003, 5084);
 
-
-
-
+-- Создаем 3 места в aircrafts.id = 1
 
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
 VALUES (1, '1A', false, true,
         (SELECT category.id FROM category WHERE category.id = 1),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 1));
-
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
 VALUES (2, '1B', false, true,
         (SELECT category.id FROM category WHERE category.id = 2),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 1));
-
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
 VALUES (3, '1C', false, true,
         (SELECT category.id FROM category WHERE category.id = 3),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 1));
 
-INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (4, '1D', false, true,
-        (SELECT category.id FROM category WHERE category.id = 4),
-        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 1));
-
-
-
+-- Создаем 3 места в aircrafts.id = 2
 
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (5, '1E', false, true,
+VALUES (4, '2A', false, true,
         (SELECT category.id FROM category WHERE category.id = 1),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 2));
-
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (6, '1F', false, true,
+VALUES (5, '2B', false, true,
         (SELECT category.id FROM category WHERE category.id = 2),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 2));
-
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (7, '2A', true, true,
-        (SELECT category.id FROM category WHERE category.id = 2),
-        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 2));
-
-INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (8, '2B', true, true,
+VALUES (6, '2C', false, true,
         (SELECT category.id FROM category WHERE category.id = 3),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 2));
 
+-- Создаем 3 места в aircrafts.id = 3
+
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (9, '2C', true, true,
+VALUES (7, '3A', true, true,
+        (SELECT category.id FROM category WHERE category.id = 1),
+        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 3));
+INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
+VALUES (8, '3B', true, true,
+        (SELECT category.id FROM category WHERE category.id = 2),
+        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 3));
+INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
+VALUES (9, '3C', true, true,
         (SELECT category.id FROM category WHERE category.id = 3),
-        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 2));
-
-
-
-
-INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (10, '2D', true, true,
-        (SELECT category.id FROM category WHERE category.id = 4),
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 3));
 
+-- Создаем 3 места в aircrafts.id = 4
+
 INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
-VALUES (11, '2E', true, true,
-        (SELECT category.id FROM category WHERE category.id = 4),
-        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 3));
+VALUES (10, '4A', true, true,
+        (SELECT category.id FROM category WHERE category.id = 1),
+        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 4));
+INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
+VALUES (11, '4B', true, true,
+        (SELECT category.id FROM category WHERE category.id = 2),
+        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 4));
+INSERT INTO seats (id, seat_number, is_near_emergency_exit, is_locked_back, category_id, aircraft_id)
+VALUES (12, '4C', true, true,
+        (SELECT category.id FROM category WHERE category.id = 3),
+        (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 4));
+
+
+-- Прямые Рейсы туда: Внуково-Омск
+-- Прямые рейсы обратно: Омск-Внуково
+-- Непрямые рейсы туда: Внуково-Курумоч - Курумоч-Омск, Внуково-Казань - Казань-Омск
+-- Непрямые рейсы обратно: Омск-Курумоч - Курумоч-Внуково, Омск-Казань - Казань-Внуково
 
 
 
+-- 1. В базе: один прямой рейс туда с наличием мест (3 свободных).
+--    Поиск: рейс туда (2023-04-01) без поиска обратного рейса
+-- 2. В базе: один прямой рейс туда и один прямой рейс обратно с наличием мест (3 свободных).
+--    Поиск: рейс туда (2023-04-01) и рейс обратно (2023-04-03)
 
-
-
-
+-- ТУДА (для 1-2 тестов)
 
 INSERT INTO flights (id, code, arrival_date, departure_date, flight_status, aircraft_id, from_id, to_id)
-VALUES (1, 'MSKOMSK', '2023-04-01 12:40:00', '2023-04-01 13:40:00', 'ON_TIME',
+VALUES (1, 'VKOOMS', '2023-04-01 12:40:00', '2023-04-01 13:40:00', 'ON_TIME',
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 1), 1, 2);
-
 INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
 VALUES (1, 500, false, false, false,
         (SELECT flights.id FROM flights WHERE flights.id = 1),
         (SELECT seats.id FROM seats WHERE seats.id = 1));
-
 INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
 VALUES (2, 600, false, false, false,
         (SELECT flights.id FROM flights WHERE flights.id = 1),
         (SELECT seats.id FROM seats WHERE seats.id = 2));
-
 INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
-VALUES (3, 650, false, false, false, (SELECT flights.id FROM flights WHERE flights.id = 1),
+VALUES (3, 650, false, false, false,
+        (SELECT flights.id FROM flights WHERE flights.id = 1),
         (SELECT seats.id FROM seats WHERE seats.id = 3));
 
-
-
-
-
-
+-- ОБРАТНО (для 2 теста)
 
 INSERT INTO flights (id, code, arrival_date, departure_date, flight_status, aircraft_id, from_id, to_id)
-VALUES (2, 'OMSKMSK', '2023-04-05 12:40:00', '2023-04-05 13:40:00', 'ON_TIME',
+VALUES (2, 'OMSVKO', '2023-04-03 12:40:00', '2023-04-03 13:40:00', 'ON_TIME',
         (SELECT aircrafts.id FROM aircrafts WHERE aircrafts.id = 2), 2, 1);
-
 INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
 VALUES (4, 500, false, false, false,
         (SELECT flights.id FROM flights WHERE flights.id = 2),
-        (SELECT seats.id FROM seats WHERE seats.id = 5));
-
+        (SELECT seats.id FROM seats WHERE seats.id = 4));
 INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
 VALUES (5, 600, false, false, false,
         (SELECT flights.id FROM flights WHERE flights.id = 2),
-        (SELECT seats.id FROM seats WHERE seats.id = 6));
-
+        (SELECT seats.id FROM seats WHERE seats.id = 5));
 INSERT INTO flight_seats (id, fare, is_registered, is_sold, is_booked, flight_id, seat_id)
 VALUES (6, 650, false, false, false,
         (SELECT flights.id FROM flights WHERE flights.id = 2),
-        (SELECT seats.id FROM seats WHERE seats.id = 7));
+        (SELECT seats.id FROM seats WHERE seats.id = 6));
+
