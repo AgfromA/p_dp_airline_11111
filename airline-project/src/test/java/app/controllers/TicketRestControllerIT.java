1bf42539f6e70298c408fc53b8dc00003d55431d
Irina Inozemtseva <rishaleva@yandex.ru>
2024-02-12 16:35:26 +0300

    #372 IT update

--- airline-project/src/test/java/app/controllers/TicketRestControllerIT.java
+++ airline-project/src/test/java/app/controllers/TicketRestControllerIT.java
@@ -1,7 +1,7 @@
 package app.controllers;
 
 import app.dto.TicketDto;
-import app.entities.Booking;
+import app.enums.BookingStatus;
 import app.mappers.TicketMapper;
 import app.repositories.TicketRepository;
 import app.services.BookingService;
@@ -41,7 +41,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
     @Autowired
     private TicketMapper ticketMapper;
 
-    // Пагинация 2.0
+    @DisplayName("shouldGetAllTickets(), return all tickets")
     @Test
     void shouldGetAllTickets() throws Exception {
         mockMvc.perform(get("http://localhost:8080/api/tickets"))
@@ -49,6 +49,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(status().isOk());
     }
 
+    @DisplayName("shouldGetAllTicketsByNullPage(), returns all tickets with the given size")
     @Test
     void shouldGetAllTicketsByNullPage() throws Exception {
         mockMvc.perform(get("http://localhost:8080/api/tickets?size=2"))
@@ -56,6 +57,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(status().isOk());
     }
 
+    @DisplayName("shouldGetAllTicketsByNullSize(), returns all tickets, if size not specified")
     @Test
     void shouldGetAllTicketsByNullSize() throws Exception {
         mockMvc.perform(get("http://localhost:8080/api/tickets?page=0"))
@@ -63,6 +65,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(status().isOk());
     }
 
+    @DisplayName("shouldGetBadRequestByPage(), return bad request-400, if page is a negative number")
     @Test
     void shouldGetBadRequestByPage() throws Exception {
         mockMvc.perform(get("http://localhost:8080/api/tickets?page=-1&size=2"))
@@ -70,6 +73,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(status().isBadRequest());
     }
 
+    @DisplayName("shouldGetBadRequestBySize(), return bad request-400, if size zero")
     @Test
     void shouldGetBadRequestBySize() throws Exception {
         mockMvc.perform(get("http://localhost:8080/api/tickets?page=0&size=0"))
@@ -77,6 +81,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(status().isBadRequest());
     }
 
+    @DisplayName("shouldGetPageTickets(), return page of tickets")
     @Test
     void shouldGetPageTickets() throws Exception {
         var pageable = PageRequest.of(0, 4);
@@ -87,7 +92,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
                         .getAllTickets(pageable.getPageNumber(), pageable.getPageSize()))));
     }
 
-    @DisplayName("createTicket(), successfully creates ticket and checks automatic id and ticketNumber generation")
+    @DisplayName("shouldCreateTicket(), successfully creates ticket and checks automatic id and ticketNumber generation")
     @Test
     void shouldCreateTicket() throws Exception {
         var newTicket = new TicketDto();
@@ -113,7 +118,7 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(jsonPath("$.ticketNumber").isNotEmpty());
     }
 
-    @DisplayName("createPaidTicket(), successfully creates ticket by booking id")
+    @DisplayName("shouldCreatePaidTicket(), successfully creates ticket by booking id")
     @Test
     void shouldCreatePaidTicket() throws Exception {
         var bookingDto = bookingService.getBookingDto(2L).get();
@@ -127,17 +132,33 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(jsonPath("$.ticketNumber").isNotEmpty());
     }
 
+    @DisplayName("shouldGetOneTicketByTicketNumber() , return 1 ticket by ticket number")
     @Test
         // fixme тест написан неправильно
-    void showTicketByBookingNumber_test() throws Exception {
-        mockMvc.perform(get("http://localhost:8080/api/tickets/")
-                        .param("ticketNumber", "SD-2222"))
+    void shouldGetOneTicketByTicketNumber() throws Exception {
+        mockMvc.perform(get("http://localhost:8080/api/tickets/{ticketNumber}", "SD-2222"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.ticketNumber").isNotEmpty())
+                .andExpect(jsonPath("$.passengerId").exists())
+                .andExpect(jsonPath("$.firstName").exists())
+                .andExpect(jsonPath("$.lastName").exists())
+                .andExpect(jsonPath("$.code").exists())
+                .andExpect(jsonPath("$.from").exists())
+                .andExpect(jsonPath("$.to").exists())
+                .andExpect(jsonPath("$.departureDateTime").exists())
+                .andExpect(jsonPath("$.arrivalDateTime").exists())
+                .andExpect(jsonPath("$.flightSeatId").exists())
+                .andExpect(jsonPath("$.seatNumber").exists())
+                .andExpect(jsonPath("$.bookingId").exists())
+                .andExpect(jsonPath("$.boardingStartTime").exists())
+                .andExpect(jsonPath("$.boardingEndTime").exists());
     }
 
+    @DisplayName("shouldUpdateTicketNumberInTicket(), update TicketNumber in ticket")
     @Test
-    void updateTicket_test() throws Exception {
+    void shouldUpdateTicketNumberInTicket() throws Exception {
         var ticketDTO = ticketMapper.toDto(ticketService.getTicketByTicketNumber("ZX-3333"));
         ticketDTO.setTicketNumber("ZX-2222");
         long numberOfTicket = ticketRepository.count();
@@ -153,12 +174,49 @@ class TicketRestControllerIT extends IntegrationTestBase {
                 .andExpect(result -> assertThat(ticketRepository.count(), equalTo(numberOfTicket)));
     }
 
+    @DisplayName("shouldUpdatePassengerAndFlightSeatInTicket(), update field Passenger and flightSeat, that assigned to a booking" +
+            "only after updating these fields in the booking")
     @Test
-    void deleteTicket_test() throws Exception {
+    void shouldUpdatePassengerAndFlightSeatInTicket() throws Exception {
+        var updatedTicket = ticketMapper.toDto(ticketService.getTicketByTicketNumber("ZX-3333"));
+        var bookingDto = bookingService.getBookingDto(3L).get();
+        bookingDto.setFlightSeatId(4L);
+        bookingDto.setPassengerId(4L);
+        bookingDto.setBookingStatus(BookingStatus.PAID);
+        bookingDto.setBookingDate(LocalDateTime.of(2023, 3, 27, 11, 20, 00));
+        bookingService.updateBooking(3L,bookingDto);
+
+        updatedTicket.setFlightSeatId(4L);
+        updatedTicket.setPassengerId(4L);
+        updatedTicket.setBookingId(3L);
+        updatedTicket.setFirstName("Merf");
+        updatedTicket.setLastName("Merfov");
+        updatedTicket.setFrom(OMS);
+        updatedTicket.setTo(VKO);
+        updatedTicket.setCode("OMSVKO");
+        updatedTicket.setSeatNumber("1D");
+        updatedTicket.setTicketNumber("ZZ-8888");
+        updatedTicket.setArrivalDateTime(LocalDateTime.of(2023, 4, 3, 7, 55, 00));
+        updatedTicket.setDepartureDateTime(LocalDateTime.of(2023, 4, 3, 7, 5, 00));
+        updatedTicket.setBoardingEndTime((LocalDateTime.of(2023, 4, 3, 6, 45, 00)));
+       updatedTicket.setBoardingStartTime((LocalDateTime.of(2023, 4, 3, 6, 25, 00)));
+
+        mockMvc.perform(patch("http://localhost:8080/api/tickets/{id}", updatedTicket.getId())
+                        .content(
+                                objectMapper.writeValueAsString(updatedTicket)
+                        )
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(updatedTicket)));
+    }
+
+    @DisplayName("shouldDeleteTicket(), delete ticket by ticket id")
+    @Test
+    void shouldDeleteTicket() throws Exception {
         Long id = 2L;
         mockMvc.perform(delete("http://localhost:8080/api/tickets/{id}", id))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-
 }
--- airline-project/src/test/resources/sqlQuery/create-ticket-before.sql
+++ airline-project/src/test/resources/sqlQuery/create-ticket-before.sql
@@ -110,3 +110,14 @@ VALUES (3, 'ZX-3333',
         (SELECT passengers.id FROM passengers WHERE passengers.id = 3),
         (SELECT flight_seats.id FROM flight_seats WHERE flight_seats.id = 3),
         (SELECT booking.id FROM booking WHERE booking.id = 3));
+
+--for update
+INSERT INTO passengers (first_name, last_name, middle_name, birth_date, gender, email, phone_number,
+                        serial_number_passport, passport_issuing_date, passport_issuing_country, id)
+VALUES ('Merf', 'Merfov', 'Merfovich', TO_DATE('2006/11/08', 'YYYY/MM/DD'), 'MALE',
+        'merfcat@mail.ru', '79121881111',
+        '0011 001860', TO_DATE('2008/01/10', 'YYYY/MM/DD'), 'Россия', 4);
+
+INSERT INTO flight_seats (id, fare, is_registered, is_sold, flight_id, seat_id)
+VALUES (4, 650, true, false, (SELECT flights.id FROM flights WHERE flights.id = 2),
+        (SELECT seats.id FROM seats WHERE seats.id = 4));
