090d0e889af3dea621e28a2a50ad0a37c4c6aac0
Irina Inozemtseva <rishaleva@yandex.ru>
2024-02-12 13:48:52 +0300

    #372 manual conflict resolved

--- airline-project/src/main/java/app/repositories/TicketRepository.java
+++ airline-project/src/main/java/app/repositories/TicketRepository.java
@@ -23,19 +23,19 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Ticket findTicketById(long id);
 
     @Query(value = "SELECT t.flightSeat.id FROM Ticket t WHERE t.passenger.id = :passengerId")
-    long [] findArrayOfFlightSeatIdByPassengerId(@Param("passengerId") long passengerId);
+    long[] findArrayOfFlightSeatIdByPassengerId(@Param("passengerId") long passengerId);
 
     @Modifying
     @Query(value = "DELETE FROM Ticket t WHERE t.passenger.id = :passengerId")
     void deleteTicketByPassengerId(@Param("passengerId") long passengerId);
 
-    List<Ticket> findByFlightId(long id);
-
     @Query(value = "SELECT ticket FROM Ticket ticket LEFT JOIN FETCH ticket.flightSeat flightSeat " +
-            "LEFT JOIN FETCH flightSeat.flight flight " +
-            "LEFT JOIN FETCH ticket.passenger WHERE flight.departureDateTime BETWEEN ?2 AND ?1")
+            "WHERE flightSeat IN (SELECT fs FROM FlightSeat fs LEFT JOIN fs.flight flight " +
+            "WHERE flight.departureDateTime BETWEEN ?2 AND ?1)")
     List<Ticket> getAllTicketsForEmailNotification(LocalDateTime departureIn, LocalDateTime gap);
+
     boolean existsByTicketNumber(String ticketNumber);
-    Optional<Ticket> findByBookingId (long bookingId);
+
+    Optional<Ticket> findByBookingId(long bookingId);
 
 }
