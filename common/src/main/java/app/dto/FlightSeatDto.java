01f85a88de2d2a832e544aa9e9613407b5a760cb
Alexsei Maskinskii <alexmaskii@gmail.com>
2024-03-14 17:54:02 +0300

    Cleaned

--- common/src/main/java/app/dto/FlightSeatDto.java
+++ common/src/main/java/app/dto/FlightSeatDto.java
@@ -1,6 +1,5 @@
 package app.dto;
 
-import app.enums.CategoryType;
 import lombok.*;
 import org.springframework.data.annotation.ReadOnlyProperty;
 
@@ -33,14 +32,4 @@ public class FlightSeatDto {
     @NotNull(message = "seatNumber shouldn't be null")
     private SeatDto seat;
 
-//    @NotNull(message = "category cannot be null")
-//    private CategoryType category;
-
-//    public CategoryType getCategory() {
-//        return category;
-//    }
-
-//    public void setCategory(CategoryType category) {
-//        this.category = category;
-//    }
 }
