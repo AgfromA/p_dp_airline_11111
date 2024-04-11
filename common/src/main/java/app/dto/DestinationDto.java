496b120f594e7a46605ed2f2e657288b92240468
Ahmed Gadjiev <akhmed.34@mail.ru>
2024-04-11 23:21:50 +0300

    add fields in DestinationDto and updated annotations in DestinationMapper

--- common/src/main/java/app/dto/DestinationDto.java
+++ common/src/main/java/app/dto/DestinationDto.java
@@ -24,4 +24,14 @@ public class DestinationDto {
     @NotBlank(message = "Field should not be empty")
     @Size(min = 2, max = 9, message = "Timezone must be between 2 and 9 characters")
     private String timezone;
+
+    @NotNull
+    private String countryName;
+
+    @NotNull
+    private String cityName;
+
+    @NotNull
+    private String airportName;
+
 }
